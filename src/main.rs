d8a7e6ebbad8970466f3a9b4feae364d0020a01d
Stéphane Magnenat <stephane@magnenat.net>
2022-10-28 20:04:58 +0200

    Applied cargo fmt.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use std::{env, fs, process::exit};
-use serde::{Deserialize};
 use anyhow::{anyhow, Result};
+use serde::Deserialize;
+use std::{env, fs, process::exit};
 
 #[derive(Deserialize)]
 struct Config {
-    auth_key: String
+    auth_key: String,
 }
 
 #[derive(Deserialize)]
 struct Translation {
     detected_source_language: String,
-    text: String
+    text: String,
 }
 
 #[derive(Deserialize)]
@@ -18,9 +18,17 @@ struct TranslationAnswer {
     translations: Vec<Translation>,
 }
 
-fn translate(text: &str, target_lang: &str, source_lang: Option<&String>, formality: Option<&String>)-> Result<()>  {
-    let filename = dirs::config_dir().ok_or_else(|| anyhow!("Cannot get config file directory"))?.join("dl-translate.toml");
-    let contents = fs::read_to_string(&filename).map_err(|old| anyhow!("Cannot open config file {:?}: {}", &filename, old))?;
+fn translate(
+    text: &str,
+    target_lang: &str,
+    source_lang: Option<&String>,
+    formality: Option<&String>,
+) -> Result<()> {
+    let filename = dirs::config_dir()
+        .ok_or_else(|| anyhow!("Cannot get config file directory"))?
+        .join("dl-translate.toml");
+    let contents = fs::read_to_string(&filename)
+        .map_err(|old| anyhow!("Cannot open config file {:?}: {}", &filename, old))?;
     let config = toml::from_str::<Config>(&contents)?;
     let client = reqwest::blocking::Client::new();
     let form = reqwest::blocking::multipart::Form::new()
@@ -29,18 +37,22 @@ fn translate(text: &str, target_lang: &str, source_lang: Option<&String>, formal
         .text("target_lang", String::from(target_lang));
     let form = match source_lang {
         Some(lang) => form.text("source_lang", String::from(lang)),
-        None => form
+        None => form,
     };
     let form = match formality {
         Some(formality) => form.text("formality", String::from(formality)),
-        None => form
+        None => form,
     };
-    let res = client.post("https://api.deepl.com/v2/translate")
+    let res = client
+        .post("https://api.deepl.com/v2/translate")
         .multipart(form)
         .send()?;
     let json = res.json::<TranslationAnswer>()?;
     for translation in json.translations {
-        println!("{} (from {})", translation.text, translation.detected_source_language);
+        println!(
+            "{} (from {})",
+            translation.text, translation.detected_source_language
+        );
     }
     Ok(())
 }
@@ -48,7 +60,10 @@ fn translate(text: &str, target_lang: &str, source_lang: Option<&String>, formal
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
-        println!("Not enough argument, usage: {} TEXT TARGET_LANG [SOURCE_LANG] [more/less (FORMALITY)]", &args[0]);
+        println!(
+            "Not enough argument, usage: {} TEXT TARGET_LANG [SOURCE_LANG] [more/less (FORMALITY)]",
+            &args[0]
+        );
         exit(1);
     }
     if let Err(err) = translate(&args[1], &args[2], args.get(3), args.get(4)) {
